0a5075d7582172d44006c7444b92dcfb439cbf79
sarahalexander <sarah.a.currie@gmail.com>
2019-06-03 10:12:03 +0200

    Remove file processor, won't need this anymore

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 var fileScanner = require('./lib/fileScanner');
-var fileProcessor = require('./lib/fileProcessor');
 var templateGenerator = require('./lib/templateGenerator');
 var resultSaver = require('./lib/resultSaver');
 
 module.exports = function html2jsProcessor(options) {
 	return fileScanner(options.filesGlob, options.fileList)
-		.then(fileProcessor(options))
 		.then(templateGenerator(options))
 		.then(resultSaver(options))
 		.catch(function (err) {
